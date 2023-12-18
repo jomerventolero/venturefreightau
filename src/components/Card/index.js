631668bd32014806b326b09eebb6966a81a39050
orekihoutaro <orekihoutaro1218@gmail.com>
2023-12-18 16:27:39 +0800

    Done some improvements

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,8 +1,9 @@
 import { motion } from 'framer-motion'
 
+
 const Card = ({icon, text}) => {
   return (
-    <motion.div className="hover:bg-blue-500 text-gray-600 hover:text-white transition-colors duration-300 ease-in-out border-[1px] border-gray-300 flex flex-col gap-2 justify-center items-center self-center w-52 h-48">
+    <motion.div className="cursor-pointer hover:bg-blue-500 text-gray-600 hover:text-white transition-colors duration-300 ease-in-out border-[1px] border-gray-300 flex flex-col gap-2 justify-center items-center self-center w-52 h-48">
       <span className="self-center align-middle">{icon}</span>
       <span className="font-bold text-center px-1">
         {text}
