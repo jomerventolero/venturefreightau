58eaf11e0d598c976167d161d66d00526c9cc8b6
orekihoutaro <orekihoutaro1218@gmail.com>
2024-01-19 09:12:11 +0800

    Added Learn More button

--- src/components/ImageSlider/index.js
+++ src/components/ImageSlider/index.js
@@ -6,6 +6,7 @@ import Image from 'next/image'
 
 
 const ImageSlider = () => {
+
   const slides = [
     {
       url: '/assets/shipglobal.jpg',
@@ -62,7 +63,7 @@ const ImageSlider = () => {
   };
 
   return (
-    <div className='max-w-[1800px] h-[580px] sm:w-3/4 w-full transition-all duration-300 m-auto p-0 sm:py-16 relative group place-items-center z-10'>
+    <div className='max-w-[1800px] h-[580px] sm:w-3/4 w-full transition-all duration-300 m-auto p-0 sm:py-16 relative group place-items-center z-10 flex flex-col'>
         
       <Image src="/assets/headsup.png" className="absolute hidden sm:block left-[15%] top-[35%] z-20" width="800" height="220" draggable={false}/>
       <Image src="/assets/headsup.png" className="absolute sm:hidden z-40 translate-y-52" width="800" height="220" draggable={false}/>
@@ -82,14 +83,16 @@ const ImageSlider = () => {
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
         className='w-full h-full bg-center bg-cover transition-all duration-500 ease-in-out fade-in hover:scale-125'
       >
-        <motion.a 
+        
+      </div>
+      <motion.div 
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           transition={{ duration: 0.8, delay: .5, type: "fadeIn" }}
-          href={slides[currentIndex].location} className="cursor-pointer hover:underline absolute z-50 top-96 sm:top-18  text-center pt-10 sm:px-40 text-white drop-shadow-2xl text-xl font-bold">{slides[currentIndex].header}
-        </motion.a>
-
-      </div>
+          className="flex flex-col content-evenly cursor-pointer absolute z-50 top-80 sm:top-18 text-center pt-10 sm:px-40 text-white drop-shadow-2xl text-xl font-bold">{slides[currentIndex].header}
+          <br/><br/>
+          <a className={`p-2 w-1/4 bg-blue-400 rounded-sm hover:bg-blue-600 mx-auto text-xs sm:text-lg shadow drop-shadow-xl ${slides[currentIndex].header === "" ? "hidden" : "block"}`} href={slides[currentIndex].location}>Learn More</a>
+      </motion.div>
 
       {/* Left Arrow */}
       <div className='group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 sm:-left-28 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
